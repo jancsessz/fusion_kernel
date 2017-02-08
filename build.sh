3d5ba073ded914d2f3604f05885827b6fa4840d9
jancsessz <jancsessz@gmail.com>
2017-02-08 15:46:40 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j8"
+KERNEL="zImage"
+DEFCONFIG="viskan_huashan_defconfig"
+
+# Kernel Details
+BASE_HC_VER="Fusion"
+VER="-1.0"
+HC_VER="$BASE_HC_VER$VER"
+
+# Vars
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=~/android/toolchain/out/arm-eabi-5.x-cortex-a15/bin/arm-eabi-
+export LOCALVERSION="-$HC_VER"
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/android/kernel/anykernel_msm"
+ZIP_MOVE="${HOME}/android/kernel/releases"
+ZIMAGE_DIR="${HOME}/android/android_kernel_sony_msm8960t/arch/arm/boot"
+
+# Functions
+function clean_all {
+		rm -rf $REPACK_DIR/scripts/zImage
+		make clean && make mrproper
+}
+
+function make_kernel {
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR/scripts/zImage
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -9 -r --exclude='*.git*' `echo $HC_VER`.zip .
+		mv  `echo $HC_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "Fusion Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$HC_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+echo "----------------------------"
+echo "Please choose your option:"
+echo "----------------------------"
+while read -p " [1]clean-build / [2]dirty-build / [3]abort " cchoice
+do
+case "$cchoice" in
+	1 )
+		HC_VER="$BASE_HC_VER$VER"
+		echo -e "${green}"
+		echo
+		echo "[..........Cleaning up..........]"
+		echo
+		echo -e "${restore}"
+		clean_all
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		break
+		;;
+	2 )
+		HC_VER="$BASE_HC_VER$VER"
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $HC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $HC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $HC_VER`.....]"
+		break
+		;;
+	3 )
+		break
+		;;
+	* )
+		echo -e "${red}"
+		echo
+		echo "Invalid try again!"
+		echo
+		echo -e "${restore}"
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
