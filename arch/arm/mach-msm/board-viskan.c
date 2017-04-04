1d43e1f2980c6d24d9567fe881577ddc1df3acc0
jancsessz <jancsessz@gmail.com>
2017-04-04 14:42:51 +0200

    Misc fixes

--- arch/arm/mach-msm/board-viskan.c
+++ arch/arm/mach-msm/board-viskan.c
@@ -3728,10 +3728,13 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.limit_temp_degC = 60,
 	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
+#ifdef CONFIG_INTELLI_THERMAL
+	.freq_control_mask = 0xf,
 #ifdef CONFIG_CPU_OVERCLOCK
 	.core_limit_temp_degC = 90,
 #else
 	.core_limit_temp_degC = 80,
+#endif
 #endif
 };
 
